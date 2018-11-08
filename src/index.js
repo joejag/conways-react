001a138ec8260da45ef45cd2284718caf3eac2e5
Joe Wright <joe@joejag.com>
2018-11-08 00:15:46 +0000

    Move components to a folder

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 
 import './index.css'
-import App from './App'
+import App from './components/App'
 import solataire from './reducers/reducers'
 
 const store = createStore(solataire, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
